7a253c3d7968cd76917ccea971010ecbf95baa2d
Kyle Winckler <kyle.winckler@gmail.com>
2016-06-23 18:15:36 -0700

    added images and created object literal structure

--- app.js
+++ app.js
@@ -11,9 +11,50 @@ function Item(name) {
   items.push(this);
 }
 
+Item.prototype.updateClicks = function() {
+  // update the clicks total of this item
+  this.clicks += 1;
+};
+
+Item.prototype.updateShown = function() {
+  // update the total of times shown when this image is selected
+  this.shown += 1;
+};
+
 // create all the item objects
 for (var index in images) {
   new Item(images[index]);
 }
 
-console.log(items);
+console.log(items); //check to make sure objects exist
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// tracker object used to change images and update all properties
+var tracker = {
+  total_clicks: 0,
+  img1: document.getElementById('img1'),
+  img2: document.getElementById('img2'),
+  img3: document.getElementById('img3'),
+
+  pickImages: function() {
+    // pick images and updated divs
+  },
+
+  updateImages: function() {
+    // update divs with seleted images
+  },
+
+  updateClickTotals: function() {
+    this.total_clicks += 1;
+  },
+
+  cancelClickListenter: function() {
+    //cancel listener after total_clicks = 15
+  }
+};
+
+console.log(tracker.total_clicks);
+tracker.updateClickTotals();
+console.log(tracker.total_clicks);
+// toDo add click event listener to items divs
