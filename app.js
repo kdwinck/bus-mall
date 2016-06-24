d5a2998d0acb7e5a50b0bc5ff0dd3ccd107b4886
Kyle Winckler <kyle.winckler@gmail.com>
2016-06-23 20:41:06 -0700

    added event listeners and method to remove event listeners after user clicks a set number of times

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var images = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 var items = [];
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // item object constructor
 function Item(name) {
   this.name = name;
@@ -20,6 +22,8 @@ Item.prototype.updateShown = function() {
   // update the total of times shown when this image is selected
   this.shown += 1;
 };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // create all the item objects
 for (var index in images) {
@@ -56,20 +60,67 @@ var tracker = {
   // update the images on the page
   updateImages: function() {
     img1.src = tracker.selectedItems[0].source;
+    tracker.selectedItems[0].updateShown();
     img2.src = tracker.selectedItems[1].source;
+    tracker.selectedItems[1].updateShown();
     img3.src = tracker.selectedItems[2].source;
+    tracker.selectedItems[2].updateShown();
+  },
+
+  clearSelectedItems: function() {
+    tracker.selectedItems = [];
   },
 
   updateClickTotals: function() {
-    this.total_clicks += 1;
+    tracker.total_clicks += 1;
+  },
+
+  updateItem1: function() {
+    tracker.selectedItems[0].updateClicks();
+    tracker.updateClickTotals();
+    tracker.clearSelectedItems();
+    tracker.doTheImageThing();
+  },
+
+  updateItem2: function() {
+    tracker.selectedItems[1].updateClicks();
+    tracker.updateClickTotals();
+    tracker.clearSelectedItems();
+    tracker.doTheImageThing();
+  },
+
+  updateItem3: function() {
+    tracker.selectedItems[2].updateClicks();
+    tracker.updateClickTotals();
+    tracker.clearSelectedItems();
+    tracker.doTheImageThing();
+  },
+
+  doTheImageThing: function () {
+    tracker.pickIndices();
+    tracker.convertIndices();
+    tracker.updateImages();
+    if (tracker.total_clicks === 15) {
+      tracker.cancelClickListenter();
+    }
   },
 
   cancelClickListenter: function() {
-    //cancel listener after total_clicks = 15
+    image1.removeEventListener('click', tracker.updateItem1);
+    image2.removeEventListener('click', tracker.updateItem2);
+    image3.removeEventListener('click', tracker.updateItem3);
   }
 };
 
-tracker.pickIndices();
-tracker.convertIndices();
-tracker.updateImages();
-// toDo add click event listener to items divs
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var image1 = tracker.img1;
+var image2 = tracker.img2;
+var image3 = tracker.img3;
+
+image1.addEventListener('click', tracker.updateItem1);
+image2.addEventListener('click', tracker.updateItem2);
+image3.addEventListener('click', tracker.updateItem3);
+
+
+tracker.doTheImageThing();
